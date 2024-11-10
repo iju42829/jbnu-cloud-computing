2d27000db79600e63aa94949448f9f61713fdc7c
iju42829 <ijud4282@gmail.com>
2024-11-11 00:18:10 +0900

    feat: 멤버 엔티티 조회 메서드 추가

--- backend/src/main/java/com/cloudbox/backend/member/service/MemberService.java
+++ backend/src/main/java/com/cloudbox/backend/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.cloudbox.backend.member.service;
 
+import com.cloudbox.backend.member.domain.Member;
 import com.cloudbox.backend.member.dto.request.MemberCreateRequest;
 
 public interface MemberService {
 
     Long signUp(MemberCreateRequest memberCreateRequest);
+
+    Member getMemberEntityByUsername(String username);
+
+    Member getMemberEntityById(Long memberId);
 }
--- backend/src/main/java/com/cloudbox/backend/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/cloudbox/backend/member/service/MemberServiceImpl.java
@@ -1,8 +1,12 @@
 package com.cloudbox.backend.member.service;
 
 import com.cloudbox.backend.common.constants.Role;
+import com.cloudbox.backend.file.dto.request.FolderCreateRequest;
+import com.cloudbox.backend.file.repository.FolderRepository;
+import com.cloudbox.backend.file.service.FolderService;
 import com.cloudbox.backend.member.domain.Member;
 import com.cloudbox.backend.member.dto.request.MemberCreateRequest;
+import com.cloudbox.backend.member.exception.MemberNotFoundException;
 import com.cloudbox.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +19,8 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final FolderService folderService;
+    private final FolderRepository folderRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -23,9 +29,26 @@ public class MemberServiceImpl implements MemberService {
                 passwordEncoder.encode(memberCreateRequest.getPassword()),
                 memberCreateRequest.getEmail(),
                 Role.USER);
+        Member savedMember = memberRepository.save(member);
 
-        Member savedId = memberRepository.save(member);
+        log.debug("회원 가입 성공: username={}", member.getUsername());
 
-        return savedId.getId();
+        folderService.createRootFolder(savedMember.getId(), new FolderCreateRequest(memberCreateRequest.getUsername()));
+
+        return savedMember.getId();
+    }
+
+    @Override
+    public Member getMemberEntityByUsername(String username) {
+        return memberRepository
+                .findByUsername(username)
+                .orElseThrow(MemberNotFoundException::new);
+    }
+
+    @Override
+    public Member getMemberEntityById(Long memberId) {
+        return memberRepository
+                .findById(memberId)
+                .orElseThrow(MemberNotFoundException::new);
     }
 }
