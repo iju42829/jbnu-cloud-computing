8a26a88d09b66d7ae3c1969e3d0143482605bbbc
JaeYun <116072376+iju42829@users.noreply.github.com>
2024-12-06 20:20:19 +0900

    파일 공유 기능 구현 (#22)

---
+++ backend/src/main/java/com/cloudbox/backend/file/dto/response/FileShareCreateResponse.java
@@ -0,0 +1,13 @@
+package com.cloudbox.backend.file.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileShareCreateResponse {
+    private String url;
+}
