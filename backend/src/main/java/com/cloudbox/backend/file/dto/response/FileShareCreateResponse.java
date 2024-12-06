3cf11ed34a10e1fa3177d5671ec0ac5dcc6a8084
iju42829 <ijud4282@gmail.com>
2024-12-06 22:39:06 +0900

    docs: 파일 및 폴더 응답 Swagger 문서 추가

--- backend/src/main/java/com/cloudbox/backend/file/dto/response/FileShareCreateResponse.java
+++ backend/src/main/java/com/cloudbox/backend/file/dto/response/FileShareCreateResponse.java
@@ -1,5 +1,6 @@
 package com.cloudbox.backend.file.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "파일 공유 생성 응답 DTO")
 public class FileShareCreateResponse {
+    @Schema(description = "파일 공유 URL")
     private String url;
 }
